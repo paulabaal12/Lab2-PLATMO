3ce29b8d084ec9c8f3a089e2594fe0263ac942f4
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-07-25 21:33:08 -0600

    Modificaciones

--- PerfilUsuario/src/main/kotlin/PerfilUsuario.kt
+++ PerfilUsuario/src/main/kotlin/PerfilUsuario.kt
@@ -1,11 +1,12 @@
+import kotlin.io.*
 data class PerfilUsuario(
     val ID:Int,
     val Nombres:String,
     val Apellidos: String,
-    val UrlPhoto: String,
+    val UrlPhoto: String?, //acepta null
     val Edad: Int,
     val Correo: String,
-    val Biografia : String,
+    val Biografia : String?,
     val Estado: String,
     val Hobbies: MutableList<Hobby> = mutableListOf()//se puede modificar despues
 ) {
@@ -13,16 +14,14 @@ data class PerfilUsuario(
         Hobbies.add(hobby)
     }
 }
-
 data class Hobby(
     val Titulo: String,
     val Descripcion: String,
-    val UrlPhoto: String
-)
+    val UrlPhoto: String)
 
 fun main(){
-    val listaUsuarios: MutableList<PerfilUsuario> = mutableListOf()
 
+    val listaUsuarios: MutableList<PerfilUsuario> = mutableListOf()
     do{
         println("Bienvenido, seleccione una opción")
         println("1.- Crear Perfil")
@@ -31,37 +30,94 @@ fun main(){
         println("4.- Agregar Hobby")
         println("5.- Salir")
         var opción = readLine()!!.toInt()
-        when(opción){
-            1->{
+        when(opción) {
+            1 -> {
                 println("Crea un perfil")
+                println("ID: ")
+                val ID = readLine()!!.toInt()
+                println("Nombres: ")
+                val Nombres = readLine().toString()
+                println("Apellidos: ")
+                val Apellidos = readLine().toString()
+                println("URL de foto, puede omitir: ")
+                val UrlPhoto = readLine().toString()
+                println("Edad: ")
+                val Edad = readLine()!!.toInt()
+                println("Correo: ")
+                val Correo = readLine().toString()
+                println("Biografía, puede omitir: ")
+                val Biografia = readLine().toString()
+                println("Estado (Activo, Pendiente, Inactivo): ")
+                val Estado = readLine().toString()
+                val usuario = PerfilUsuario(ID, Nombres, Apellidos, UrlPhoto, Edad, Correo, Biografia, Estado)
+                listaUsuarios.add(usuario)
+                print("\n${Nombres} ${Apellidos}  tu perfil se ha creado exitosamente\n")
+
             }
-            2->{
-                println("Buscar Usuario")
+
+            2 -> {
+                println("Buscar Usuario, Ingrese Nombres/ Apellidos o ID")
+                val confirmacion = readLine().orEmpty()
+                var found = false
+                for (usuario in listaUsuarios) {
+                    if (usuario.Nombres.equals(confirmacion, ignoreCase = true) ||
+                        usuario.Apellidos.equals(confirmacion, ignoreCase = true) ||
+                        usuario.ID.toString() == confirmacion
+                    ) {
+                        found = true
+                        println("Perfil encontrado:")
+                        println("ID: ${usuario.ID}")
+                        println("Nombres: ${usuario.Nombres}")
+                        println("Apellidos: ${usuario.Apellidos}")
+                        println("URL de foto: ${usuario.UrlPhoto}")
+                        println("Edad: ${usuario.Edad}")
+                        println("Correo: ${usuario.Correo}")
+                        println("Biografía: ${usuario.Biografia}")
+                        println("Estado: ${usuario.Estado}")
+                        println("Hobbies: ")
+                        for (hobby in usuario.Hobbies) {
+                            println("- Título: ${hobby.Titulo}")
+                            println("  Descripción: ${hobby.Descripcion}")
+                            println("  URL: ${hobby.UrlPhoto}")
+                        }
+                        break
+                    }}}
+
+            3 -> {
+                println("Eliminar Perfil/Usuario, debe ingresar el ID")
+                val idingresado = readln().toIntOrNull()
+                val borrar = listaUsuarios.find { it.ID == idingresado }
+                if (idingresado != null){
+                listaUsuarios.remove(borrar)
+                println("Perfil eliminado/borrado")
+            }else {
+                println("No existe ningun usuario con ese ID")
             }
-            3->{
-                println("Eliminar Perfil/Usuario")
-            }
+        }
             4->{
-                println("Agregar Hobby")
+                println("Agregar Hobby,Ingrese Apellido/Nombre o ID ")
+                val respuestausuario = readLine().toString()
+                val agregarhobby = listaUsuarios
+                if (agregarhobby.equals(respuestausuario)) {
+                    println("Titulo (hobby: ")
+                    val Titulo = readLine().toString()
+                    println("Descripción (hobby: ")
+                    val Descripcion = readLine().toString()
+                    println("URL (hobby: ")
+                    val UrlPhoto = readLine().toString()
+
+                    val hobby = Hobby(Titulo, Descripcion, UrlPhoto)
+                    println("Se agrego tu hobby")
+                }else{
+                    println("No se pudo agregar tu hobby")
+                }
             }
             5->{
-                println("Eso fue todo!!")
+               println("Eso fue todo!!")
             }
             else->{
                 println("Está opción no es valida :(")
             }
         }
     } while(opción !=5)
-
-    //USUARIOS PREDETERMINADOS
-    listaUsuarios.add(
-        PerfilUsuario(
-            161998,"Charles", "Leclerc", "image/jpeg;base64", 25, "lec1612@gmail.com",
-            "Piloto de F1" , "Activo" )
-    )
-    listaUsuarios.add(
-        PerfilUsuario(
-            131989,"Taylor", "Swift", "image/jpeg;base22", 33, "taytay13@gmail.com",
-            "Industria Musical, Cantante" , "Activo" )
-    )
 }
